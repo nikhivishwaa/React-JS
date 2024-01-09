8a4eb4371d91f6864385f6c9213bb9473c8aa8aa
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2024-01-09 23:17:31 +0530

    Writing first JSX for my react app

--- Readme.md
+++ Readme.md
@@ -20,3 +20,22 @@ npx create-react-app my-first-app
 ```
 
 ###### Above command helps to create a react app without installing the create-react-app package
+
+
+##### Run the React app
+
+```
+cd my-first-app
+npm start
+```
+
+
+###### JSX Fragment - if we have to return adjacent elements
+```
+<></>
+```
+
+###### write JS expression in JSX between {}
+
+###### img, br, hr tag  writtern as <img/> <br/> <hr/> because all tags must be
+###### terminated in JSX
--- my-first-app/src/App.js
+++ my-first-app/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+        
+//           My First React Project
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+let name = "Nikhil Vishwakarma"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+      <h1>This is Editing in JSX</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor, eveniet adipisci praesentium repudiandae totam mollitia saepe inventore nisi esse qui provident distinctio nam excepturi ipsam! Quod accusamus aspernatur eveniet libero?</p>
+      <div className="copyight">
+        <hr />
+        <p>&copy; {name}</p>
+      </div>
     </div>
+    </>
   );
 }
 
